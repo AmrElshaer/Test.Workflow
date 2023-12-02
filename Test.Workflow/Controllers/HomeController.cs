514b0dcb462c71ad26f1d70369837e053c5d81a1
Amr Elshaer <50411156+AmrElshaer@users.noreply.github.com>
2023-12-02 13:18:17 +0200

    add version 2

---
+++ Test.Workflow/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test.Workflow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("update version 2");
+        }
+    }
+}
